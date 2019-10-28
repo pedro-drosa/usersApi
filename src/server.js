beda29b6005dfd7f0ee971bbb2f07e04b61a0408
Pedro Juraci <pedro_drosa@hotmail.com>
2019-10-28 12:47:18 -0300

    database connection

--- src/server.js
+++ src/server.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+
+mongoose.connect('mongodb+srv://myusername:mypassword@cluster0-mzspv.mongodb.net/usersapi?retryWrites=true&w=majority',{
+    useNewUrlParser:true,
+    useUnifiedTopology: true,
+});
 
 app.listen('8080',()=>{
     console.log('this server is running');
