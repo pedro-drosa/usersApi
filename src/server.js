22913db3be31e20dfd40d53b13ade7d0fe8e700c
pedro-drosa <pedro_drosa@hotmail.com>
2023-10-18 16:57:37 -0300

    refactor: configure environment variables

--- src/server.js
+++ src/server.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const dotenv = require('dotenv');
 
+dotenv.config();
 const app = express();
 
 app.use(express.json());
 app.use(routes);
 
-mongoose.connect('mongodb+srv://myusername:mypassword@cluster0-mzspv.mongodb.net/usersapi?retryWrites=true&w=majority',{
-    useNewUrlParser:true,
-    useUnifiedTopology: true,
+const { MONGO_URI, APP_BASE_URL } = process.env;
+
+mongoose.connect(`${MONGO_URI}`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-app.listen('8080',()=>{
-    console.log('this server is running');
-})
+const APP_PORT = process.env.PORT || 3000;
+
+app.listen(`${APP_PORT}`, () => {
+  console.log(`server is running on ${APP_BASE_URL}:${APP_PORT}`);
+});
