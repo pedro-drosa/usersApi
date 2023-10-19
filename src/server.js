539adc4e824777c2d561f609511ecf510d1dc7e2
pedro-drosa <pedro_drosa@hotmail.com>
2023-10-19 06:02:04 -0300

    refactor(server.js): separate the server instance into a new file

--- src/server.js
+++ src/server.js
@@ -1,12 +1,5 @@
 require('dotenv/config');
-require('./lib/mongoose');
-const express = require('express');
-const routes = require('./routes');
-
-const app = express();
-
-app.use(express.json());
-app.use(routes);
+const { app } = require('./app');
 
 const { APP_BASE_URL } = process.env;
 const APP_PORT = process.env.PORT || 3000;
